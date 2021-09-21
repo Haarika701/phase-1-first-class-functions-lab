0fa705c8360aaa252f2000d633d39cf5e7650c02
Katelyn <katelynjewel@gmail.com>
2021-09-21 15:20:06 -0700

    Katelyn driver, second test passed, third was not

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const returnFirstTwoDrivers = function(newArr){
    return newArr.slice(0,2);
     console.log(array1.slice(0,2));
     
-    
 }
 let array1 = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
 returnFirstTwoDrivers(array1);
-    
+
+const returnLastTwoDrivers = function(newArr){
+    return newArr.slice(2, 4);
+}
+
+// function selectingDrivers(a, b){
+//     a = returnFirstTwoDrivers(); 
+//     b = returnLastTwoDrivers();
+//     return newArr = (a, b);
+// }
