1c78614a6f3adc3e72afe2cb5c124bbe0fa9a653
HaarikaR <hramadugu@gmail.com>
2021-09-21 14:42:26 -0700

    Haarika, driver , 1st test passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(newArr){
+   return newArr.slice(0,2);
+    console.log(array1.slice(0,2));
+    
+    
+}
+let array1 = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+returnFirstTwoDrivers(array1);
+    
